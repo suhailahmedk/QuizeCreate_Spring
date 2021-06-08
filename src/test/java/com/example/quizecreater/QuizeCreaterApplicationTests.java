1d725d9052bb847af25fab4324eadacf18d36853
Suhail Ahmed <suhailahmed.se17@iba-suk.edu.pk>
2021-06-08 16:30:23 +0500

    connection_with_spring

---
+++ src/test/java/com/example/quizecreater/QuizeCreaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizecreater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizeCreaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
