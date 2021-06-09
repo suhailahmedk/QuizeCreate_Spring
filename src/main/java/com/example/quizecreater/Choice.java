e48b15d022c465b04bff890e98fa40dbc549b018
Suhail Ahmed <suhailahmed.se17@iba-suk.edu.pk>
2021-06-09 07:35:54 +0500

    up to Controller

---
+++ src/main/java/com/example/quizecreater/Choice.java
@@ -0,0 +1,22 @@
+package com.example.quizecreater;
+
+public class Choice {
+    String choice;
+    String correct;
+
+    public String getChoice() {
+        return choice;
+    }
+
+    public void setChoice(String choice) {
+        this.choice = choice;
+    }
+
+    public String getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(String correct) {
+        this.correct = correct;
+    }
+}
