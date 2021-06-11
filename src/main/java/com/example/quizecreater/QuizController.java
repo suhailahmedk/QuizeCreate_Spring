ad9592a2b3bfab773496443f0cd0cc74aa949d80
Suhail Ahmed <suhailahmed.se17@iba-suk.edu.pk>
2021-06-11 18:26:38 +0500

    update4

--- src/main/java/com/example/quizecreater/QuizController.java
+++ src/main/java/com/example/quizecreater/QuizController.java
@@ -5,15 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QuizController {
-    @GetMapping("/addQuiz")
-    public String QuizForm(Model model) {
-        model.addAttribute("addQuiz", new Quiz());
-        return "addQuiz";
-    }
 
-    @PostMapping("/addQuiz")
-    public String QuizSubmit(@ModelAttribute Quiz quiz, Model model) {
-        model.addAttribute("addQuiz", quiz);
-        return "result";
-    }
 }
